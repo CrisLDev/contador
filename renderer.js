5c26984d83d242c41dca3bea2797d80997ad155c
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-01-31 16:04:08 -0500

    listo el error de que no cogia el archivo numero para guardar el content

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
 const { match } = require('assert');
 const fs = require('fs');
+const path = require('path');
+const file = path.join(__dirname, "./numero.txt");
 
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
@@ -9,7 +11,7 @@ const fs = require('fs');
 // process.
 
 function listo() {
-    var numero = fs.readFileSync('numero.txt');
+    var numero = fs.readFileSync(file);
     document.getElementById("valorr").innerHTML = numero;
 }
 
@@ -18,7 +20,7 @@ listo();
 document.getElementById('resetear').addEventListener("click", resetear)
 
 function sumar() {
-    var numero = fs.readFileSync('numero.txt');
+    var numero = fs.readFileSync(file);
     var algo2 = parseFloat(numero);
     return numero = dividir(multiplicar(algo2) + multiplicar(0.05));
 }
@@ -33,7 +35,7 @@ function dividir (a) {
 
 function resetear() {
     const cero = 0;
-    fs.writeFileSync('numero.txt', cero, (err) => {
+    fs.writeFileSync(file, cero, (err) => {
         if(err) throw err;
     })
     document.getElementById("valorr").innerHTML = 0;
@@ -43,7 +45,7 @@ document.addEventListener('keydown', logKey);
     function logKey(e) {
       if (`${e.code}` == "ArrowRight") {
           var valor = sumar();
-          fs.writeFileSync('numero.txt', valor, (err) => {
+          fs.writeFileSync(file, valor, (err) => {
               if(err) throw err;
           })
           document.getElementById("valorr").innerHTML = valor;
@@ -51,7 +53,7 @@ document.addEventListener('keydown', logKey);
       }
       if(`${e.code}` == "ArrowUp"){
             const cero = 0;
-            fs.writeFileSync('numero.txt', cero, (err) => {
+            fs.writeFileSync(file, cero, (err) => {
                 if(err) throw err;
             })
             document.getElementById("valorr").innerHTML = 0;
